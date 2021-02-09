71b681c9fa07b5b2996bb6dc11527f7157fee1e1
fr0staman <77665176+fr0staman@users.noreply.github.com>
2021-02-09 19:11:50 +0200

    Додав форму з оформленням клієнта

--- taskmanager/main/models.py
+++ taskmanager/main/models.py
@@ -5,6 +5,7 @@ class Task(models.Model):
     title = models.CharField('Назва', max_length=64)
     task = models.TextField('Опис')
 
+
 class Orders(models.Model):
     P = models.CharField('Прізвище', max_length=16)
     I = models.CharField('Ім`я', max_length=16)
@@ -14,6 +15,7 @@ class Orders(models.Model):
     zona = models.CharField('Фотозона', max_length=32)
     DodPoslugu = models.CharField('Додаткові послуги', max_length=64)
     Descrip = models.TextField('Опис')
+
 
 
     def __str__(self):
